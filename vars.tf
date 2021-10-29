fdf2a81a01a26ef1383ffc482f0e0a38a3455145
surulink <38012582+surulink@users.noreply.github.com>
2021-10-28 21:55:56 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 // instance.tf variables
 variable "instance_name" {}
-variable "node_count" {default = "2"}
+variable "node_count" {}
 variable "instance_machine_type" {}
 variable "instance_zone" {}
 variable "instance_image" {}
