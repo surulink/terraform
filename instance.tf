f31c10056051c67bf7d55174a6b0bcbc4e5dc067
surulink <38012582+surulink@users.noreply.github.com>
2021-10-31 22:32:31 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -23,6 +23,13 @@ resource "google_compute_instance" "main" {
   allow_stopping_for_update = true
 }
 
+resource "google_compute_subnetwork" "public-subnetwork" {
+name = "terraform-subnetwork"
+ip_cidr_range = "10.2.0.0/16"
+region = "us-central1"
+network = google_compute_network.vpc_network.name
+}
+
 resource "google_compute_firewall" "default" {
   name    = "allow-jenkins-rule"
   network = "default"
